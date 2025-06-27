67c8a5338089ce8a4b1bd30b97c6ca88ac848cf2
swarajmutalik <swarajmutalik@gmail.com>
2025-06-27 17:15:49 +0530

    EventListener

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 "use strict";
 
-document.querySelector(".message").textContent = " Correct Number";
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number";
+  }
+});
