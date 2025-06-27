eef6d1b581a382887d02c0fbcc712a818ab69a07
swarajmutalik <swarajmutalik@gmail.com>
2025-06-27 12:40:11 +0530

    Manipulation of Elements

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-'use strict';
+"use strict";
+
+document.querySelector(".message").textContent = " Correct Number";
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
