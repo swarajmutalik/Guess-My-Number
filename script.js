2b0cc5890f97261cc950741df792150ad90c1b60
swarajmutalik <swarajmutalik@gmail.com>
2025-06-28 17:10:52 +0530

    Game Logic

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 "use strict";
 
+const number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector(".number").textContent = number;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "No number";
+  } else if (guess === number) {
+    document.querySelector(".message").textContent = "Correct Number";
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too High";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+    }
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too Low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = 0;
+    }
   }
 });
