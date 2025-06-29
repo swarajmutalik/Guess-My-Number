65714c376d9ade50703d1fbba6fe75548786b5c0
swarajmutalik <swarajmutalik@gmail.com>
2025-06-29 18:25:16 +0530

    styling manipulated

--- script.js
+++ script.js
@@ -3,17 +3,26 @@
 const number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector(".number").textContent = number;
-
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
+  // when there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "No number";
-  } else if (guess === number) {
+    document.querySelector(".message").textContent = "No Number";
+  }
+
+  // when the player wins the game
+  else if (guess === number) {
     document.querySelector(".message").textContent = "Correct Number";
-  } else if (guess > number) {
+    document.querySelector(".number").textContent = number;
+
+    document.querySelector("body").style.background = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+  }
+
+  // when the guess is too high
+  else if (guess > number) {
     if (score > 1) {
       document.querySelector(".message").textContent = "Too High";
       score--;
@@ -28,7 +37,8 @@ document.querySelector(".check").addEventListener("click", function () {
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = 0;
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
     }
   }
 });
